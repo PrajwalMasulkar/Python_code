4867cd3013de740b00a8f641bdbb2fefe9574e81
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-01-31 23:40:03 +0530

    'new'

---
+++ prog19.py
@@ -0,0 +1,8 @@
+#Read 2 String from command Line and concatenate them
+
+print("Enter First name:")
+fn=input()
+print("Enter Last name:")
+ln = input()
+name=fn+" "+ln
+print("Full Name:  " + name)
