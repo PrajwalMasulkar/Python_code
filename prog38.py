17d1ace3e10774d18c787c5796d68b3c0b7c3ded
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-02-04 21:40:46 +0530

    'new'

---
+++ prog38.py
@@ -0,0 +1,6 @@
+#dict()constructor to make a dictionary:
+
+thisdict=dict(apple="green",banana="yellow",cherry="red")
+#note that keywords are not string literals
+#note the use of equals rather than coln for the assignment
+print(thisdict)
