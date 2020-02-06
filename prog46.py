a0ba9879e02bcc869e9d9a75a1f896b2b019a2f8
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-02-06 23:06:44 +0530

    'new'

---
+++ prog46.py
@@ -0,0 +1,10 @@
+#Check a no +ve or -ve.If +ve then check greater then 1000 or not
+#using If-Elif-Else Statement
+a=120
+if a>0:
+ if a>1000:
+  print("Greater Than 1000")
+ else:
+  print("Less than 1000")
+else:
+  print("-ve No")
