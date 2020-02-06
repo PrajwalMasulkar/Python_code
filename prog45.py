b35c6264f2ab306171f8c6b2d55c343935f0ddef
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-02-06 23:06:28 +0530

    'new'

---
+++ prog45.py
@@ -0,0 +1,14 @@
+#Check vowel or consonent using If-Elif-Else Statement
+ch='e'
+if ch=='a':
+ print("Vowel")
+elif ch=='e':
+ print("Vowel")
+elif ch=='i':
+ print("Vowel")
+elif ch=="o":
+ print("Vowel")
+elif  ch=='u':
+ print("Vowel")
+else:
+ print("Consonent")
