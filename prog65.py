0e03f0e5b025265f83c5e5012c825c7f85336f21
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-02-10 21:46:15 +0530

    'new'

---
+++ prog65.py
@@ -0,0 +1,24 @@
+#Array Operations
+
+names=["Dev","Amit","Ram"]
+for a in names:
+ print(a)
+ 
+#Reverse Array Element
+names.reverse()
+print(names)
+#Sort Array Element
+names.sort()
+print(names)
+#Array Length
+len=len(names)
+print(len)
+#Append Element
+names.append("sunil")
+print(names)
+# Remove Element using index
+names.pop(1)
+print(names)
+# Remove Element using Value
+names.remove("Ram")
+print(names)
