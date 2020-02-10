d68cad0b15e9169d6e063988fc6f544d4c0b490f
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-02-10 21:47:08 +0530

    'new'

---
+++ prog67.py
@@ -0,0 +1,11 @@
+#Store Emp Info using Class and Object
+
+class emp:
+ eid=101
+ ename="Ashish"
+ sal=12990
+ 
+ob=emp()
+print("Emp ID: "+str(ob.eid))
+print("Emp Name: "+ob.ename)
+print("Emi Sal: "+str(ob.sal))
