5c9d39027f28035ec14b14a27c1e2c35913f49f0
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-02-19 00:00:13 +0530

    'new'

---
+++ prog99.py
@@ -0,0 +1,10 @@
+#Return list of table in our databases:
+import mysql.connector
+
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="",database="pythondb")
+mycursor=mydb.cursor()
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+ print(x)
